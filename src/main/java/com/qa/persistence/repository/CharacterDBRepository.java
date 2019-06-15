d5a0892b675fca2db63014f3d3883bcbd9514328
Tania <tdrydendiaz@gmail.com>
2019-06-15 23:51:02 +0100

    repo udates

--- src/main/java/com/qa/persistence/repository/CharacterDBRepository.java
+++ src/main/java/com/qa/persistence/repository/CharacterDBRepository.java
@@ -22,18 +22,24 @@ public class CharacterDBRepository implements CharacterRepository {
 	private EntityManager manager;
 	
 	
-	@Transactional(TxType.REQUIRED)
+//	@Transactional(TxType.REQUIRED)
 	@Override
 	public String getAllCharacters() {
 		Query query = manager.createQuery("SELECT a FROM Characters a ORDER BY characterID");
 
-//		@SuppressWarnings("unchecked")
+
 		Collection<Character> character = (Collection<Character>) query.getResultList();
 
 		return util.getJSONForObject(character);
 	}
+	
+	@Override
+	public String getACharacter(int characterID) {
+		return util.getJSONForObject(manager.find(Character.class, characterID));
+	}
 
 	@Override
+	@Transactional(TxType.REQUIRED)
 	public String createCharacter(String character) {
 		new JSONUtil();
 		Character util = JSONUtil.getObjectForJSON(character, Character.class);
@@ -43,6 +49,7 @@ public class CharacterDBRepository implements CharacterRepository {
 
 
 	@Override
+	@Transactional(TxType.REQUIRED)
 	public String deleteCharacter(int characterID) {
 		Character character= manager.find(Character.class, characterID);
 	    manager.remove(character);
@@ -50,15 +57,27 @@ public class CharacterDBRepository implements CharacterRepository {
 	}
 
 	@Override
+	@Transactional(TxType.REQUIRED)
 	public String updateCharacter(int characterID, String character) {
-		// TODO Auto-generated method stub
+//		Character charToUpdate = manager.find(Character.class, characterID);
+//		Character updatedChar = util.getObjectForJSON(character, Character.class);
+//		if (charToUpdate != null) {
+//			charToUpdate.setName(updatedChar.getCharacterName());
+//			charToUpdate.setCategory(updatedChar.getActorName());
+//			charToUpdate.setQuantity(updatedChar.getPowerRating());
+//			charToUpdate.setThreshold(updatedChar.getMorality());
+//			charToUpdate.setExpiryDate(updatedChar.getGender());
+//			charToUpdate.setExpiryDate(updatedChar.getHomeworld());
+//			
+//			manager.persist(charToUpdate);
+//			return "{\"message\": \"Character successfully updated\"}";
+//		} else {
+//			return "{\"message\": \"cannot find Character\"}";
+//		}
 		return null;
+		
 	}
 
-	@Override
-	public String getACharacter(int characterID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
