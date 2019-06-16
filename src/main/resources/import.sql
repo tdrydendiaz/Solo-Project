9e780d290709e7ff1b94db19f3d83b46b11be05a
Tania <tdrydendiaz@gmail.com>
2019-06-16 12:56:55 +0100

    import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,6 @@ INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) 
 INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (2, 'Attack of the Clones', 2002, 142, 'PG', 65);
 INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (3, 'Revenge of the Sith', 2005, 140, 'PG', 80);
 INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (4, 'A new Hope', 1977, 125, 'PG', 93);
+INSERT INTO Character (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (1, 'Luke Skywalker', 'Mark Hamill',90, 'Hero', 'Male', 'Tatooine');
+INSERT INTO Character (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (2,'Darth Vader', 'David Prowse',85, 'Villain', 'Male', 'Tatooine');
+INSERT INTO Character (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (3,'Leia Organa', 'Carrie Fisher',70, 'Hero', 'Female', 'Alderaan);
