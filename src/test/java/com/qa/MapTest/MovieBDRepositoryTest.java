2afaafa8071d18b1edaf24a015eba332a128ee08
Tania <tdrydendiaz@gmail.com>
2019-06-17 17:20:54 +0100

    mokito tests

---
+++ src/test/java/com/qa/MapTest/MovieBDRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.qa.MapTest;
+
+import javax.management.Query;
+import javax.persistence.EntityManager;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.qa.persistence.repository.MovieDBRepository;
+import com.qa.util.JSONUtil;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MovieBDRepositoryTest {
+	@InjectMocks
+	private MovieDBRepository repo;
+
+	@Mock
+	private EntityManager manager;
+
+	@Mock
+	private Query query;
+
+	private JSONUtil util;
+	
+	private static final String MOCK_DATA_ARRAY = "[{\"movieID\":1,\"title\":\"The Phantom Menance\",\"releaseYear\":\"1999\",\"runTime\":\"133\",\"certification\":\"PG\",\"rating\":\"54\"}]";
+
+	private static final String MOCK_OBJECT =  "{\"movieID\":1,\"title\":\"The Phantom Menance\",\"releaseYear\":\"1999\",\"runTime\":\"133\",\"certification\":\"PG\",\"rating\":\"54\"}";
+	
+
+	
+
+	
+	
+		
+	}	
+	
+
+
