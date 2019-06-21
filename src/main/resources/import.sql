ae8fc39080253686d44355cacb49a5e1344f32e3
Tania <tdrydendiaz@gmail.com>
2019-06-21 13:18:57 +0100

    sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,11 @@
-INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (1,'The Phantom Menance', 1999, 133, 'PG', 54);
+INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (1,'The Phantom Menace', 1999, 133, 'PG', 54);
 INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (2, 'Attack of the Clones', 2002, 142, 'PG', 65);
 INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (3, 'Revenge of the Sith', 2005, 140, 'PG', 80);
-INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (4, 'A new Hope', 1977, 125, 'PG', 93);
+INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (4, 'A New Hope', 1977, 125, 'PG', 93);
+INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (5, 'The Empire Strikes Back', 1980, 124, 'PG', 45);
+INSERT INTO Movie (movieID, title, releaseYear, runTime, certification, rating) VALUES (6, 'Return of the Jedi', 1983, 132, 'PG', 50);
 
-
-INSERT INTO CHARACTER (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (1, 'Luke Skywalker', 'Mark Hamill', 56, 'Hero', 'Male', 'Tatooine');
+INSERT INTO CHARACTER (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (1, 'Luke Skywalker', 'Mark Hamill', 85, 'Hero', 'Male', 'Tatooine');
+INSERT INTO CHARACTER (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (2, 'Darth Vader', 'David Prowse', 90, 'Villain', 'Male', 'Tatooine');
+INSERT INTO CHARACTER (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (3, 'R2-D2', 'Jimmy Vee', 20, 'Hero', 'N/A', 'Naboo');
+INSERT INTO CHARACTER (characterID, characterName, actorName, powerRating, morality, gender, homeworld) VALUES (4, 'Leia Organa', 'Carrie Fisher', 30, 'Hero', 'Female', 'Alderaan');
